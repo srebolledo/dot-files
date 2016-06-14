f99ac7202c8cfc94812513c7645719cf5e40737c
Stephan Rebolledo <stephan.rebolledo@dena.com>
2016-06-14 10:10:15 -0400

    Update to dot-files

--- tmux/install.sh
+++ tmux/install.sh
@@ -4,4 +4,5 @@ BASE_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 #Patching open, pbcopy, etc on MacOSX
 brew install reattach-to-user-namespace
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
